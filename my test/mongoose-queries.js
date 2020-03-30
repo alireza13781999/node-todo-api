c43c5331d6fc48be4d0af86d84a09309f9ad943c
alireza13781999 <alirezanemati1999@gmail.com>
2020-03-30 16:21:33 +0430

    queries test

---
+++ my test/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '5e773a4a29037e1b20a24c2c11';
+
+if(!ObjectId.isValid(id)) {
+    console.log('Id is not valid');
+}
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     if(!todos) {
+//         return console.log('Id not found...');
+//     }
+//     console.log('Todos ' ,todos);
+// });
+
+
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     if(!todo) {
+//         return console.log('Id not found...');
+//     }
+//     console.log('Todo ' ,todo);
+// });
+
+
+
+Todo.findById(id).then((todo) => {
+    if(!todo) {
+        return console.log('Id not found...');
+    }
+    console.log('Todo by id ' ,todo);
+}).catch((err) => console.log(err));
