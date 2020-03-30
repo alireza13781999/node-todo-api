fe3707c5048ce84d8eeafc0840ab55a73fe8c182
alireza13781999 <alirezanemati1999@gmail.com>
2020-03-30 13:49:15 +0430

    get api and test

--- server/server.js
+++ server/server.js
@@ -27,6 +27,21 @@ app.post('/todos', (req, res) => {
 
 });
 
+
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+
+
 app.listen(port, () => {
     console.log('Listening on ', port);
 });
+
+
+module.exports = {app};
