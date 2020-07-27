b1b1d2c05949b480b62d3d491e699ed74edb3bb5
alireza13781999 <alirezanemati1999@gmail.com>
2020-07-27 15:13:59 +0430

    test todos/;id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,9 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    _id: new ObjectID() ,
+    text: "first test"
+}, {
+    _id: new ObjectID() ,
+    text: "second test"
+}];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => done());
@@ -30,5 +38,19 @@ describe('POST /todos', () => {
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+          .get(`/todos/${todos[0]._id.toHexString}`)
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.todo.text).toBe(todos[0].text);
+          });
+          .end(done);
     });
 });
