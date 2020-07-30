ae0c61ec4f904de4d4bfa9ee4bdeb72587f56aff
alireza13781999 <alirezanemati1999@gmail.com>
2020-07-30 17:05:57 +0430

    heroku

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/users');
 
 var app = express();
-var port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
