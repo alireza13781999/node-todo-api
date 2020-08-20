d5212d9a812cccf77584a62567744cf137fd813e
alireza13781999 <alirezanemati1999@gmail.com>
2020-08-20 17:03:18 +0430

    Update API

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo}     = require('./models/todo');
 var {User}     = require('./models/users');
@@ -71,7 +72,33 @@ app.delete('/todos/:id', (req,res) => {
     });
 });
 
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
 
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send('{}');
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+
+        if(!todo) {
+            return res.status(404).send({});
+        }
+        res.status(200).send({todo});
+        
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+
+});
 
 app.listen(port, () => {
     console.log('Listening on ', port);
