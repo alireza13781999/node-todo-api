c1e6e55a7473752ec378bd16ecac9d0ed1a4d809
alireza13781999 <alirezanemati1999@gmail.com>
2020-09-02 15:54:08 +0430

    authenticate route

--- server/models/user.js
+++ server/models/user.js
@@ -41,7 +41,7 @@ UserSchema.methods.toJSON = function () {
 
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
-    var access = 'auth';
+    var access = 'auth'; 
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
     //user.tokens.push({access, token});
@@ -50,6 +50,27 @@ UserSchema.methods.generateAuthToken = function () {
     return user.save().then(() => {
         return token;
     });
+};
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+
+        decoded = jwt.verify(token, 'abc123');
+
+    
+    } catch (e) {
+        return Promise.reject();
+    }
+   
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+    
 };
 
 var User = mongoose.model('User', UserSchema);
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo}     = require('./models/todo');
 var {User}     = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -105,12 +106,17 @@ app.post('/users', (req, res) => {
     var user = new User(body);
 
     user.save().then(() => {
-        user.generateAuthToken();
+        return user.generateAuthToken();
     }).then((token) => {
         res.header('x-auth', token).send(user);
     }).catch((err) => {
         res.status(400).send(err);
      });
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
 });
 
 
